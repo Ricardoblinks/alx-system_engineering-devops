4f026e23de750bcbd9b46d0a1261650e6f3bae71
ricardoblinks <ricardoblinks1@gmail.com>
2023-06-27 13:51:25 +0100

    first puppet manifest

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# This Puppet manifest creates a file in /tmp called "school" with specific permissions and ownership
+
 file { '/tmp/school':
   ensure  => 'file',
-  content => 'I love Puppet',
+  content => "I love Puppet\n",
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
